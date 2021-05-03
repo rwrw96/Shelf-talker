ea3d50509f74db40b5a6b5db7727d350a75617f6
rwrw96 <kazuki137@gmail.com>
2021-05-03 11:34:30 +0000

    [fix]ログアウト時新規登録画面に遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) 
   end
+  
+#　ログアウト時新規登録画面に遷移
+  def after_sign_out_path_for(resource)
+     new_user_registration_path 
+  end
 end
