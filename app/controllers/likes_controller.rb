ae0fc7f0b1251f67235ec5ce4bb56dfc230f9159
rwrw96 <kazuki137@gmail.com>
2021-05-03 07:52:53 +0000

    [create]user/inde,show,follow機能,reviewに対しlike機能

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+    
+  def create
+    @review = Review.find(params[:id])
+    Like.create(user_id: current_user.id, review_id: @review.id)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    Like.find_by(user_id: current_user.id, review_id: @review.id).destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,31 @@
 class UsersController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
+  def follows
+    user = User.find(params[:id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.all
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name,:introduction)
   end
 end
+
