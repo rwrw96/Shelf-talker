616457ab2342d06ff01e9c33bd1a3b225e0946f2
rwrw96 <kazuki137@gmail.com>
2021-05-06 13:58:29 +0000

    deproy setting

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,10 +4,7 @@ class MoviesController < ApplicationController
   Tmdb::Api.key(ENV['API_KEY'])
   
   def index
-    if Review.find_by(rate: 5).exists?
-      top_review = Review.where(rate: 5)
-      @top_reviews = top_review.order("RANDOM()").limit(1)
-    end
+
   end
   
   def show 
