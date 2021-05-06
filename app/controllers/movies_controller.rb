d15d7324f50027e7a10c0d8b81fd2d6da64ebc3a
rwrw96 <kazuki137@gmail.com>
2021-05-06 08:59:37 +0000

    API_KEY setting

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   require "themoviedb-api"
   Tmdb::Api.language("ja")
-  Tmdb::Api.key("")
+  Tmdb::Api.key(ENV['API_KEY'])
   
   def index
     top_review = Review.where(rate: 5)
