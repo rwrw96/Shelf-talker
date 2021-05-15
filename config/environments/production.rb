0c632f8e875b86da38f6452eb5ee4975b916fa27
rwrw96 <kazuki137@gmail.com>
2021-05-15 02:03:22 +0000

    [create]メール認証機能実装

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,14 +94,14 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  # address:              'smtp.gmail.com',
-  # port:                 587,
-  # domain:               'gmail.com',
-  # user_name:            'watabetesuto@gmail.com',
-  # password:             'jybqjxsgpwmultlq',
-  # authentication:       'plain',
-  # enable_starttls_auto: true }
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'watabetesuto@gmail.com',
+  password:             'jybqjxsgpwmultlq',
+  authentication:       'plain',
+  enable_starttls_auto: true }
   
   config.action_mailer.default_url_options = { protocol: 'http' , host: '52.194.83.6' }
   # mail setting
