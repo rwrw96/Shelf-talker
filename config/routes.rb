f7a3fd8475d909383719c5b221bc8a9396572fe7
rwrw96 <kazuki137@gmail.com>
2021-05-15 04:36:25 +0000

    [create]メール認証機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -26,11 +26,12 @@ Rails.application.routes.draw do
     resources :users, only: [:index,:destroy]
   end
   
-  resource :homes, only: [:about, :welcome]
   
   
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
   get "movies/:id" => "movies#show", as: "detail"
+  get "homes/about" => "homes#about"
+  get "homes/welcome" => "homes#welcome"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
