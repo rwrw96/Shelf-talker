90bbbf7907344645c87391227b00e4c73bedd330
rwrw96 <kazuki137@gmail.com>
2021-05-15 02:11:23 +0000

    [create]メール認証機能実装

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,19 +102,18 @@ Rails.application.configure do
   password:             'jybqjxsgpwmultlq',
   authentication:       'plain',
   enable_starttls_auto: true }
-  
-  config.action_mailer.default_url_options = { protocol: 'http' , host: '52.194.83.6' }
-  # mail setting
-  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.smtp_settings = {
-    enable_starttls_auto: true,
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: 'smtp.gmail.com',
-    user_name: ENV['user_name'],
-    password: ENV['password'], 
-    authentication: 'login'
-  }
+  # config.action_mailer.default_url_options = { protocol: 'http' , host: '52.194.83.6' }
+  # config.action_mailer.raise_delivery_errors = true
+
+  # config.action_mailer.smtp_settings = {
+  #   enable_starttls_auto: true,
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: 'smtp.gmail.com',
+  #   user_name: ENV['user_name'],
+  #   password: ENV['password'], 
+  #   authentication: 'login'
+  # }
   
 end
