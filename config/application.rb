58af84f8490d746a5a77bdf05d81405b57a6acb9
rwrw96 <kazuki137@gmail.com>
2021-05-20 02:56:26 +0000

    [fix]review created_atを表示

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,8 @@ module ShelfTalker
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    # タームゾーン日本時間に設定
+    config.time_zone = 'Tokyo'
   end
 end
