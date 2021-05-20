9f9d4b91681f5a1b18bd90bbc0b78c202b354fc0
rwrw96 <kazuki137@gmail.com>
2021-05-20 01:25:17 +0000

    [create]例外発生時、ルートパスに画面遷移するよう修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,16 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     new_user_registration_path
   end
+  
+  # ルーティングエラーの場合
+  rescue_from ActionController::RoutingError, with: :render_404
+  # 値が存在しない場合
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+  def render_404(e = nil)
+      Tmdb::Api.key(ENV['API_KEY'])
+      # loggor.info内のe.messageがnilならばレンダリング
+      logger.info "Rendering 404 with exception: #{e.message}" if e
+      render "movies/index"
+  end
 end
